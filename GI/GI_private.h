98705f40be8be52fbf76c0fb3fd1965b7c0a8431
Ahmed Rabea <85747078+AhmedRabea1@users.noreply.github.com>
2024-03-07 15:46:23 +0200

    Add files via upload

---
+++ GI/GI_private.h
@@ -0,0 +1,17 @@
+/*
+ * GI_private.h
+ *
+ * Created: 3/3/2024 1:11:12 PM
+ *  Author: lenovo
+ */ 
+
+
+#ifndef GI_PRIVATE_H_
+#define GI_PRIVATE_H_
+
+#define SREG			 *(volatile u8 *) 0x5F
+#define I							7 
+
+
+
+#endif /* GI_PRIVATE_H_ */
